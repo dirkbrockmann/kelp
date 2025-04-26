46e986bda23d180ef7357ec3b77085d1c7bc7b1a
Dirk Brockmannn <brockmann.dirk@gmail.com>
2025-04-26 19:30:37 +0200

    prep release

--- dist/index.es.js
+++ dist/index.es.js
@@ -12,7 +12,7 @@ const oo = {
   controls_size: { width: 480, height: 480 },
   display_class: " tw:p-0",
   controls_class: "tw:p-0",
-  container_class: "tw:font-sans tw:font-light tw:grid tw:grid-cols-1 tw:sm:grid-cols-2 tw:sm:gap-8 tw:px-1 tw:sm:p-0 tw:m-8"
+  container_class: "tw:font-sans tw:font-light tw:font-light tw:grid tw:grid-cols-1 tw:sm:grid-cols-2 tw:sm:gap-8 tw:px-1 tw:sm:p-0 tw:m-8"
 };
 function fn(t, n) {
   return t == null || n == null ? NaN : t < n ? -1 : t > n ? 1 : t >= n ? 0 : NaN;
